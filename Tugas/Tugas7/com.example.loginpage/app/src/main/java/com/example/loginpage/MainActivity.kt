b30c00bbb20d42b7e514bcd4981faf815ac7e024
Ahmad Zaky Ash Shidqi <151065728+cashewwww14@users.noreply.github.com>
2025-06-23 19:03:18 +0700

    Create MainActivity.kt

---
+++ Tugas/Tugas7/com.example.loginpage/app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.loginpage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.loginpage.ui.theme.LoginPageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginPageTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
